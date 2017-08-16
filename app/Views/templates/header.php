cddeb625dec0098960e0322647d73b6f45533540
Maskharashvili Andy <webdev.andym@gmail.com>
2017-08-16 17:41:10 +0300

    fix

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -6,7 +6,7 @@
     <meta basePath= "<?php $url->url(); ?>">
     <meta name = 'curentPage' value = ''>
     <link rel="shortcut icon" href="<?php echo $url->url(); ?>favicon.ico" type="image/x-icon">
-    <title>QTT Panel [<?php echo $data->user; ?>]</title>
+    <title>QTT Panel &#9749; <?php echo $data->user; ?></title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" media="bogus" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=PT+Sans&amp;subset=latin,cyrillic-ext" rel="stylesheet" type="text/css" media="bogus">
